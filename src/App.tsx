bbe6b6cdebce3ba668fcec27487e230c13cc12b9
Justin Capalbo <capalbo.justin@gmail.com>
2019-04-20 09:50:22 -0400

    Query with an input field and hook dependency array

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,36 @@ type Hit = {
     title: string;
 }
 
-const fetchUrl = "http://hn.algolia.com/api/v1/search?query=redux";
 
 const App: React.FC = () => {
+    const [query, setQuery] = useState("redux");
     const [data, setData] = useState<HitResponse>({ hits: [] });
 
     useEffect(() => {
+        const fetchUrl = `http://hn.algolia.com/api/v1/search?query=${query}`;
         const fetchHits = async () => { 
             const result = await axios.get<HitResponse>(fetchUrl);
             setData(result.data);
         };
         
         fetchHits();
-    }, []);
+    }, [query]);
 
     return (
-        <ul>
-            {data.hits.map(item => (
-                <li key={item.objectID}>
-                    <a href={item.url}>{item.title}</a>
-                </li>
-            ))}
-        </ul>
+        <>
+            <input 
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <ul>
+                {data.hits.map(item => (
+                    <li key={item.objectID}>
+                        <a href={item.url}>{item.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 };
 
