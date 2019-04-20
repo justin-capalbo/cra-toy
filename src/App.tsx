1a3ca8b7eb25a3b16dee0cc6b1127bc5449f5957
Justin Capalbo <capalbo.justin@gmail.com>
2019-04-20 09:39:15 -0400

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+type Item = {
+    objectID: string;
+    url: string;
+    title: string;
+}
+
 const App: React.FC = () => {
+    const [data, setData] = useState<{ hits: Item[] }>({ hits: [{ objectID: "test", url: "https://google.com", title: "test item" }] });
+
     return (
-        <>
-            <h1>React App</h1>
-        </>
+        <ul>
+            {data.hits.map(item => (
+                <li key={item.objectID}>
+                    <a href={item.url}>{item.title}</a>
+                </li>
+            ))}
+        </ul>
     );
 };
 
